bbdb414a48ec92a3c89a3fa8046e2f34643aa526
Junhyung-Choi <junhyung9571@naver.com>
2021-11-11 01:13:41 +0900

    ADD Scene Change

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum ESceneList
+{
+    Main,
+    Flower,
+    Wood,
+    Ceramic
+}
+
+public class ChangeScene : MonoBehaviour
+{
+    public ESceneList scene;
+    public void SceneChange()
+    {
+        switch(scene)
+        {
+            case ESceneList.Main:
+                SceneManager.LoadScene("Main");
+                break;
+            case ESceneList.Flower:
+                SceneManager.LoadScene("Flower");
+                break;
+            case ESceneList.Wood:
+                SceneManager.LoadScene("Wood");
+                break;
+            case ESceneList.Ceramic:
+                SceneManager.LoadScene("Ceramic");
+                break;
+        }
+    }
+}
