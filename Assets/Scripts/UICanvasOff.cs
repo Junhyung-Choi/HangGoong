672873372372d73e98624bc7349708551e926a69
Junhyung-Choi <junhyung9571@naver.com>
2021-12-02 10:38:13 +0900

    UPDATE Main Scene, Flower Scene

---
+++ Assets/Scripts/UICanvasOff.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICanvasOff : MonoBehaviour
+{
+    public GameObject Canvas;
+    public void turnoff()
+    {
+        Canvas.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
