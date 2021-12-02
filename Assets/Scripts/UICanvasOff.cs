4640f3269247346f089cb684fa1652a40327c20e
Junhyung-Choi <junhyung9571@naver.com>
2021-12-02 15:14:16 +0900

    UPDATE Sotdae table

--- Assets/Scripts/UICanvasOff.cs
+++ Assets/Scripts/UICanvasOff.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class UICanvasOff : MonoBehaviour
 {
+    public static bool isOpened = false;
     public GameObject Canvas;
     public GameObject onCanvas;
     public void turnoff()
     {
+        isOpened = true;
         Canvas.SetActive(false);
         onCanvas.SetActive(true);
     }
@@ -16,7 +18,10 @@ public class UICanvasOff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(UICanvasOff.isOpened)
+        {
+            turnoff();
+        }
     }
 
     // Update is called once per frame
